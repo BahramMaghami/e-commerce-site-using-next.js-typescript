e8b2d2dfed8e1071c02d5f2742d58a324e9a6b7a
bahram maghami <bahrammaghami193@gmail.com>
2026-07-20 22:22:13 +0330

    added the form to the sign in page with label and input componentwhich installed from shadcn

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from '@/lib/constants'
 import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import CredentialsSignInForm from './credentials-signing-form'
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -34,7 +35,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* { from here } */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   )
