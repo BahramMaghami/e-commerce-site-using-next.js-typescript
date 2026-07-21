0ecfeefa2a5f35cf96b74ab7a0cc902981ea1373
bahram maghami <bahrammaghami193@gmail.com>
2026-07-21 15:12:10 +0330

    added callbackUrl for shipping addresses

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -18,11 +18,15 @@ export const metadata: Metadata = {
   description: 'Sign in to your account',
 }
 
-const SignInPage = async () => {
-  
+const SignInPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>
+}) => {
+  const { callbackUrl } = await props.searchParams
+
   const session = await auth()
+
   if (session) {
-    redirect('/')
+    redirect(callbackUrl || '/')
   }
 
   return (
