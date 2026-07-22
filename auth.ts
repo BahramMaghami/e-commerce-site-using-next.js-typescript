aeb46c8a57019b6bcbe54c011534daa6926e9ec8
bahram maghami <bahrammaghami193@gmail.com>
2026-07-23 01:20:26 +0330

    added the role and name key to the token and session

--- auth.ts
+++ auth.ts
@@ -60,12 +60,35 @@ export const config = {
       // set the user ID from the token to the session object
       session.user.id = token.sub
 
+      session.user.role = token.role
+      session.user.name = token.name
+
+
       // if there is a update to the user name, update the session object
       if (trigger === 'update') {
         session.user.name = user.name
       }
 
       return session
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role
+
+        // If user has no name then use the email
+
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0]
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          })
+        }
+      }
+      return token
     },
   },
 } satisfies NextAuthConfig
