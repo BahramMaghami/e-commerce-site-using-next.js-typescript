4b75c1be3e9d8e06b422dd6b53e9f25295a34c2f
bahram maghami <bahrammaghami193@gmail.com>
2026-07-25 00:54:59 +0330

    made the ui smooth when u click the cart buttons

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -3,54 +3,61 @@
 import { Cart, CartItem } from '@/types/index'
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
-import { Plus, Minus } from 'lucide-react'
+import { Plus, Minus, Loader } from 'lucide-react'
 import { toast } from 'sonner'
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.action'
+import { useTransition } from 'react'
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter()
 
+  const [isPending, startTransition] = useTransition()
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item)
+    startTransition(async () => {
+      const res = await addItemToCart(item)
 
-    if (res && !res.success) {
-      toast.error(res.message, {
-        className: '!bg-red-500',
+      if (res && !res.success) {
+        toast.error(res.message, {
+          className: '!bg-red-500',
+        })
+        return
+      }
+
+      // Handle success add to cart
+      toast('', {
+        position: 'bottom-right',
+        description: res.message,
+        action: {
+          label: 'Go To Cart',
+          onClick: () => router.push('/cart'),
+        },
+        className: '!flex !gap-3',
       })
-      return
-    }
-
-    // Handle success add to cart
-    toast('', {
-      position: 'bottom-right',
-      description: res.message,
-      action: {
-        label: 'Go To Cart',
-        onClick: () => router.push('/cart'),
-      },
-      className: '!flex !gap-3',
     })
   }
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId)
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId)
 
-    if (res && !res.success) {
-      toast.error(res.message, {
-        className: '!bg-red-500',
+      if (res && !res.success) {
+        toast.error(res.message, {
+          className: '!bg-red-500',
+        })
+        return
+      }
+
+      // Handle success remove to cart
+      toast('', {
+        position: 'bottom-right',
+        description: res.message,
+        action: {
+          label: 'Go To Cart',
+          onClick: () => router.push('/cart'),
+        },
+        className: '!flex !gap-3',
       })
-      return
-    }
-
-    // Handle success remove to cart
-    toast('', {
-      position: 'bottom-right',
-      description: res.message,
-      action: {
-        label: 'Go To Cart',
-        onClick: () => router.push('/cart'),
-      },
-      className: '!flex !gap-3',
     })
   }
 
@@ -61,11 +68,19 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div className="flex gap-4 justify-center items-center">
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2 text-xl">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
@@ -75,7 +90,12 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
       type="button"
       onClick={handleAddToCart}
     >
-      <Plus /> Add To Cart
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}{' '}
+      Add To Cart
     </Button>
   )
 }
