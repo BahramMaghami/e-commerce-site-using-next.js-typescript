1e25c96937a5154d8034981c1838753cf040f2b6
bahram maghami <bahrammaghami193@gmail.com>
2026-07-27 20:20:15 +0330

    created server action for payment method

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,6 @@
 'use server'
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
@@ -10,7 +11,7 @@ import { prisma } from '@/lib/prisma'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { formatError } from '../utils'
 import { ShippingAddress } from '@/types'
-import { da } from 'zod/v4/locales'
+import z, { success } from 'zod'
 
 // Sign in the usere with credentials
 export async function signInWithCredentials(
@@ -109,6 +110,31 @@ export async function updateUserAddress(data: ShippingAddress) {
       success: true,
       message: 'User updated successfully!',
     }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>,
+) {
+  try {
+    const session = await auth()
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    })
+
+    if (!currentUser) throw new Error('User not found')
+
+    const paymentMethod = paymentMethodSchema.parse(data)
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    })
+
+    return { success: true, message: 'User updated successfully' }
   } catch (error) {
     return { success: false, message: formatError(error) }
   }
