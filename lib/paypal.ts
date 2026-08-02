cdf2abd1b782bb4d3886bc90d517e1bb8401fae8
bahram maghami <bahrammaghami193@gmail.com>
2026-08-02 14:51:10 +0330

    created capturePayment and createOrder function for paypal

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,10 +1,50 @@
 import { POST } from '@/app/api/auth/[...nextauth]/route'
+import { currency } from './validators'
 
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com'
 
 console.log(base)
 
-export const paypal = {}
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken()
+    const url = `${base}/v2/checkout/orders`
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'USD',
+              value: price,
+            },
+          },
+        ],
+      }),
+    })
+
+    return handleResponse(response)
+  },
+  capturePayment: async function captrurePayment(orderId: string) {
+    const accessToken = await generateAccessToken()
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorizaion: `Bearer ${accessToken}`,
+      },
+    })
+    return handleResponse(response)
+  },
+}
 
 // Generate access token
 async function generateAccessToken() {
@@ -22,9 +62,13 @@ async function generateAccessToken() {
     },
   })
 
+  const jsonData = await handleResponse(response)
+  return jsonData.access_token
+}
+
+async function handleResponse(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json()
-    return jsonData.access_token
+    return response.json()
   } else {
     const errorMessega = await response.text()
     throw new Error(errorMessega)
