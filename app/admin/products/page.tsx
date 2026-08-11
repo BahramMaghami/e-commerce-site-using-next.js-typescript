b0c6f291b2d7acda7a56a4e767cf82040b890bcc
bahram maghami <bahrammaghami193@gmail.com>
2026-08-11 12:38:44 +0330

    created the admin/products page and the action for that

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { getAllProducts } from '@/lib/actions/product.action'
+import { formatCurrency, formatId } from '@/lib/utils'
+
+const AdminProductsPage = async (props: {
+  searchParams: Promise<{ page: string; query: string; category: string }>
+}) => {
+  const searchParams = await props.searchParams
+  const page = Number(searchParams.page) || 1
+  const category = searchParams.category || ''
+  const searchText = searchParams.query || ''
+
+  const products = await getAllProducts({ query: searchText, page, category })
+
+  console.log(products)
+
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Products</h1>
+      </div>
+    </div>
+  )
+}
+
+export default AdminProductsPage
