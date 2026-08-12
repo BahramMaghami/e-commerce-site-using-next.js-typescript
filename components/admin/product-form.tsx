02b8ae104e2062d4063dfb344b28a3877f414597
bahram maghami <bahrammaghami193@gmail.com>
2026-08-12 20:42:31 +0330

    created the ui for the uploadthing which u can upload the product images and it will be fully added to db

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -26,6 +26,9 @@ import slugify from 'slugify'
 import { Button } from '../ui/button'
 import { Textarea } from '../ui/textarea'
 import { createProduct, updateProduct } from '@/lib/actions/product.action'
+import { UploadButton } from '@/lib/uploadthing'
+import { Card, CardContent } from '../ui/card'
+import Image from 'next/image'
 
 const ProductForm = ({
   type,
@@ -80,6 +83,8 @@ const ProductForm = ({
       }
     }
   }
+
+  const images = form.watch('images')
 
   return (
     <form
@@ -237,6 +242,45 @@ const ProductForm = ({
         </div>
         <div className="upload-field flex flex-col md:flex-row gap-5">
           {/* Imges */}
+          <Controller
+            name="images"
+            control={form.control}
+            render={({ fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="w-full">
+                <FieldLabel htmlFor="form-rhf-demo-images">Images</FieldLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="product image"
+                          className="w-20 h-20 object-cover rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                    </div>
+                  </CardContent>
+                </Card>
+                <UploadButton
+                  endpoint={'imageUploader'}
+                  onClientUploadComplete={(res: { url: string }[]) => {
+                    form.setValue('images', [...images, res[0].url])
+                  }}
+                  onUploadError={(error: Error) => {
+                    toast.error(`ERROR! ${error.message}`, {
+                      className: '!bg-red-500',
+                    })
+                  }}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
         </div>
         <div className="upload-field">{/* isFeatured */}</div>
         <div>
