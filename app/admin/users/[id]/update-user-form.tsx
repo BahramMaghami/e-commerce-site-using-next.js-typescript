1c1ca3053dd1990ea469145f94ade522a3e2b4b3
bahram maghami <bahrammaghami193@gmail.com>
2026-08-13 13:09:08 +0330

    created the ui for edit user page but the action is what we go next

---
+++ app/admin/users/[id]/update-user-form.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { updateUserSchema } from '@/lib/validators'
+import { useRouter } from 'next/navigation'
+import z from 'zod'
+import { toast } from 'sonner'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import {
+  SelectItem,
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { USER_ROLES } from '@/lib/constants'
+import { Button } from '@/components/ui/button'
+
+const UpdateUserForm = ({
+  user,
+}: {
+  user: z.infer<typeof updateUserSchema>
+}) => {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof updateUserSchema>>({
+    resolver: zodResolver(updateUserSchema),
+    defaultValues: user,
+  })
+
+  const onSubmit = () => {}
+
+  return (
+    <form
+      id="form-rhf-demo"
+      method="POST"
+      onSubmit={form.handleSubmit(onSubmit)}
+    >
+      <FieldGroup>
+        {/* Email */}
+        <div>
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="w-full">
+                <FieldLabel htmlFor="form-rhf-demo-email">Email</FieldLabel>
+                <Input
+                  {...field}
+                  id="form-rhf-demo-email"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Enter user email"
+                  autoComplete="off"
+                  disabled
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+        </div>
+        {/* Name */}
+        <div>
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="w-full">
+                <FieldLabel htmlFor="form-rhf-demo-name">Name</FieldLabel>
+                <Input
+                  {...field}
+                  id="form-rhf-demo-name"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Enter user name"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+        </div>
+
+        {/* Role */}
+        <div>
+          <Controller
+            name="role"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid} className="w-full">
+                <FieldLabel htmlFor="form-rhf-demo-role">Role</FieldLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  value={field.value.toString()}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a role" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {USER_ROLES.map((role) => (
+                      <SelectItem
+                        key={role}
+                        value={role.charAt(0).toUpperCase() + role.slice(1)}
+                      >
+                        {role.charAt(0).toUpperCase() + role.slice(1)}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+        </div>
+        <div className="flex-between">
+          <Button
+            type="submit"
+            className={'w-full'}
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? 'Submitting...' : 'Update User'}
+          </Button>
+        </div>
+      </FieldGroup>
+    </form>
+  )
+}
+
+export default UpdateUserForm
